aaa5abd2c179227023e4ea4f40b2aa3fdcb88798
Fahim Abdullah Danial <105501133+Danial864@users.noreply.github.com>
2022-06-03 00:06:09 +0600

    Create leap year.c

---
+++ codeforces/leap year.c
@@ -0,0 +1,22 @@
+include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter the year : ");
+
+    scanf("%d",&n);
+    if(n%4==0 && n%100!=0)
+    {
+        printf("%d is leap year",n);
+    }
+    else if (n%4==0 && n%100==0 && n%400==0)
+    {
+        printf("%d is leap year",n);
+    }
+
+    
+    else 
+    {
+        printf("%d is not leap year",n);
+    }
+}
